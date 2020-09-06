e6b1c7e0fb20514cc8426885c545aacd138fc9a2
邹大清 <2295957879@qq.com>
2020-09-06 15:44:19 +0800

    账户金额组件

--- src/pages/Welcome/component/Account/index.jsx
+++ src/pages/Welcome/component/Account/index.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import { Statistic, Badge, Divider } from 'antd';
+import style from './style.less';
+import grandUrl from '@/assets/images/grand.png';
+import protectUrl from '@/assets/images/protect.png';
 
 const Account = () => {
   return (
-    <div></div>
-  )
-}
+    <div className={style.account}>
+      <Statistic
+        value={596}
+        precision={2}
+        valueStyle={{ fontSize: 30, fontWeight: 'bold' }}
+        title="账户金额"
+        prefix="￥"
+      />
+      <div className={style.guarante}>
+        <img src={protectUrl} height={24} /> 资金保障
+      </div>
+      <div className={style.income}>
+        <Badge status="success" /> 今日收支 ￥0.00
+        <Badge status="processing" /> 昨日收支 ￥0.00
+      </div>
+      <Divider className={style.divider} />
+      <p>
+        <img src={grandUrl} height={18} />
+        <span className={style.total}>近30天累计收支 ￥5,682.25</span>
+      </p>
+    </div>
+  );
+};
 export default Account;
--- src/pages/Welcome/index.jsx
+++ src/pages/Welcome/index.jsx
@@ -1,20 +1,40 @@
 import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
 import { Card, Row, Col } from 'antd';
-import { TodoList } from './component/index'
+import { TodoList, Accout, Audit, InfoBar } from './component/index';
 
 export default () => (
   <PageContainer>
     <Row gutter={24}>
+      <Col span={6}>
+        {/* 账户资产 */}
+        <Card hoverable>
+          <Accout />
+        </Card>
+      </Col>
+      <Col span={10}>
+        {/* 工作审批 */}
+        <Card hoverable>
+          <Audit />
+        </Card>
+      </Col>
+      {/* 信息通知 */}
+      <Col span={8}>
+        <Card hoverable>
+          <InfoBar />
+        </Card>
+      </Col>
+    </Row>
+    <Row gutter={24} style={{ marginTop: 24 }}>
+      {/* 代办事项 */}
       <Col span={16}>
         <Card title="代办事项" hoverable>
           <TodoList />
         </Card>
       </Col>
+      {/* 经营数据 */}
       <Col span={8}>
-        <Card title="经营数据" hoverable>
-
-        </Card>
+        <Card title="经营数据" hoverable></Card>
       </Col>
     </Row>
   </PageContainer>
